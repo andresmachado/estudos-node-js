d3cbd504ddb4584aeff29d3a9b8759dac544d267
andresmachado <csantos.machado@gmail.com>
2015-11-13 15:17:49 -0300

    Adding new files

---
+++ text_sync.js
@@ -0,0 +1,8 @@
+'use strict'
+var fs  = require('fs');
+
+for(var i = 1; i <= 5; i++){
+  var file = "sync-txt" + i + ".txt";
+  var out = fs.writeFileSync(file, "Hello Node.js!");
+  console.log(file);
+}
